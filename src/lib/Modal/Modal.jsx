f334ae3cef65527d8a9917e9857a9479a0e7efe5
Simon Tessard <simon.tessard@gmail.com>
2023-04-11 15:19:56 +0200

    Prop types added + moved close button

--- src/lib/Modal/Modal.jsx
+++ src/lib/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import './modal.css'
 
 export function Modal({ children, isOpen, titleText, closeText }) {
@@ -15,14 +16,29 @@ export function Modal({ children, isOpen, titleText, closeText }) {
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <div className="modal-title">
               <h1>{titleText}</h1>
+            </div>
+            {children}
+            <div className="modal-close-container">
               <button className="modal-close" onClick={handleClose}>
                 {closeText}
               </button>
             </div>
-            {children}
           </div>
         </div>
       )}
     </>
   )
+}
+
+Modal.propTypes = {
+  children: PropTypes.array,
+  isOpen: PropTypes.bool.isRequired,
+  titleText: PropTypes.string,
+  closeText: PropTypes.string,
+}
+
+Modal.defaultProps = {
+  isOpen: false,
+  titleText: 'Title',
+  closeText: 'Close',
 }
