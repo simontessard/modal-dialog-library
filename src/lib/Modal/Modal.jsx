f14850de457720119bfdd99ad78d3348fc8837ed
Simon Tessard <simon.tessard@gmail.com>
2023-04-15 22:51:00 +0200

    Add: logo + css changes

--- src/lib/Modal/Modal.jsx
+++ src/lib/Modal/Modal.jsx
@@ -14,6 +14,22 @@ export function Modal({ children, isOpen, titleText, closeText }) {
       {isModalOpen && (
         <div className="modal-overlay" onClick={handleClose}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div class="image">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+                <g id="SVGRepo_iconCarrier">
+                  {' '}
+                  <path
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke-width="1.5"
+                    stroke="#000000"
+                    d="M20 7L9.00004 18L3.99994 13"
+                  ></path>{' '}
+                </g>
+              </svg>
+            </div>
             <div className="modal-title">
               <h1>{titleText}</h1>
             </div>
