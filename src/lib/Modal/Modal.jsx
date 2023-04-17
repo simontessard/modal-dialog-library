90c56dfa8b6d68ba59ba25486ca86bc470908ef8
Simon Tessard <simon.tessard@gmail.com>
2023-04-17 21:21:24 +0200

    Removed useless lines

--- src/lib/Modal/Modal.jsx
+++ src/lib/Modal/Modal.jsx
@@ -26,39 +26,37 @@ export function Modal({ children, isOpen, titleText, closeText, style }) {
   }
 
   return (
-    <>
-      {isModalOpen && (
-        <div className="modal-overlay" onClick={handleClose}>
-          <div className="modal-content" style={contentStyle} onClick={(e) => e.stopPropagation()}>
-            <div className="image" style={logoStyle}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
-                <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
-                <g id="SVGRepo_iconCarrier">
-                  {' '}
-                  <path
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="1.5"
-                    stroke="#000000"
-                    d="M20 7L9.00004 18L3.99994 13"
-                  ></path>{' '}
-                </g>
-              </svg>
-            </div>
-            <h1 className="modal-title" style={titleStyle}>
-              {titleText}
-            </h1>
-            {children}
-            <div className="modal-close-container" style={closeContainerStyle}>
-              <button className="modal-close" style={closeButtonStyle} onClick={handleClose}>
-                {closeText}
-              </button>
-            </div>
+    isModalOpen && (
+      <div className="modal-overlay" onClick={handleClose}>
+        <div className="modal-content" style={contentStyle} onClick={(e) => e.stopPropagation()}>
+          <div className="image" style={logoStyle}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
+              <g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g>
+              <g id="SVGRepo_iconCarrier">
+                {' '}
+                <path
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="1.5"
+                  stroke="#000000"
+                  d="M20 7L9.00004 18L3.99994 13"
+                ></path>{' '}
+              </g>
+            </svg>
+          </div>
+          <h1 className="modal-title" style={titleStyle}>
+            {titleText}
+          </h1>
+          {children}
+          <div className="modal-close-container" style={closeContainerStyle}>
+            <button className="modal-close" style={closeButtonStyle} onClick={handleClose}>
+              {closeText}
+            </button>
           </div>
         </div>
-      )}
-    </>
+      </div>
+    )
   )
 }
 
@@ -70,7 +68,6 @@ Modal.propTypes = {
 }
 
 Modal.defaultProps = {
-  isOpen: false,
   titleText: 'Title',
   closeText: 'Close',
 }
