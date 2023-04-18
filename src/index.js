1c5159795585a5bce6ef0fe9ed46ac8add1d502a
Simon Tessard <simon.tessard@gmail.com>
2023-04-18 18:25:44 +0200

    Modal is now published in NPM and well working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Modal } from './lib/Modal/Modal'
+import { Modal } from './lib/components/Modal'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 const text = React.createElement('p', { className: 'red' }, 'Hello, this is a test text !')
